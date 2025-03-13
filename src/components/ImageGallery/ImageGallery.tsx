ff8d6ba2ebf6721849ff138f0a7742b64fce6f40
tinkared <tinkared@gmail.com>
2025-03-13 12:28:02 +0100

    deployFixes imgTypes

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -2,7 +2,7 @@ import styles from "./ImageGallery.module.css";
 
 interface Image {
 id: string;
-urls: { small: string };
+urls: { small: string; regular: string };
 alt_description?: string;
 }
 
@@ -13,17 +13,17 @@ onImageClick: (image: Image) => void;
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ items, onImageClick }) => {
 return (
-<ul className={styles.gallery}>
-{items.map((item) => (
-    <li
-    key={item.id}
-    className={styles.galleryItem}
-    onClick={() => onImageClick(item)}
-    >
-    <img src={item.urls.small} alt={item.alt_description || "Image"} />
-    </li>
+    <ul className={styles.gallery}>
+    {items.map((item) => (
+        <li
+        key={item.id}
+        className={styles.galleryItem}
+        onClick={() => onImageClick(item)}
+        >
+        <img src={item.urls.small} alt={item.alt_description || "Image"} />
+        </li>
 ))}
-</ul>
+    </ul>
 );
 };
 
